ea005252e6ab5eca819fba385c8f485282e28b54
KhurshidYakubov <khurshid.yakubov@gmail.com>
2020-05-10 05:25:02 +0500

    kh

--- task_1-2.py
+++ task_1-2.py
@@ -1,42 +1,65 @@
+import matplotlib.pylab as plt
 import cv2
-import numpy as np 
+import numpy as np
 
 
-cap=cv2.VideoCapture("Texas 4K - Sunrise Drive - Scenic Drive - USA_1.mp4")
-
-while(cap.isOpened()):
-
-	ret, frame=cap.read()
-
-	if ret is False:
-		print("End of the video")
-		break
-
-	#task 1: Denoising the video frames using GuassianBlur function
-	blurred=cv2.GaussianBlur(frame, (5,5), 0)
-	#task 2: converting 3-chanel color image into 1chanel gray image	
-	blurred_gray=cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-	
-	#reducing features from image using binarization
-	edges = cv2.Canny(blurred_gray,100,200)
-	#creating kernel	
-	kernel_1=np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-	#filtering using filter2D
-	sharpened_1=cv2.filter2D(edges, -1, kernel_1)
-
-	
-	
-
-	
-	cv2.imshow('sharpened_1', sharpened_1)
-	
-	
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    # channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
 
 
+def drow_the_lines(img, lines):
+    img = np.copy(img)
+    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
-	q=cv2.waitKey(1)
-	if q==ord('q'):
-		print("q is pressed:quit")
-		break
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(blank_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=10)
+
+    img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
+    return img
 
 
+# = cv2.imread('road.jpg')
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def process(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    region_of_interest_vertices = [
+        (100, height),
+        (width / 2, 250),
+        (600, height)
+    ]
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    canny_image = cv2.Canny(gray_image, 50, 240)
+    cropped_image = region_of_interest(canny_image,
+                                       np.array([region_of_interest_vertices], np.int32), )
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho=3,
+                            theta=np.pi / 180,
+                            threshold=50,
+                            lines=np.array([]),
+                            minLineLength=40,
+                            maxLineGap=100)
+    image_with_lines = drow_the_lines(image, lines)
+    return image_with_lines
+
+
+cap = cv2.VideoCapture('video.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    h,w,d = frame.shape
+    frame = cv2.resize(frame, ( int(w/2), int(h/2) ))
+    frame = process(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
